029280706ea86fc26ac00c0fd1cb65c4761f39fc
NerdiOrg <38194792+NerdiOrg@users.noreply.github.com>
2021-09-29 13:55:51 -0700

    Update index.php

--- index.php
+++ index.php
@@ -11,4 +11,13 @@ How are you?");
 
 // Also, you can use "/lb/"  to force a line break:
 $fakephp->run("s /lb/Hello2 b!");
+
+
+// Now we need to test the ability to process our code blocks, which is when you pass array(s) that contain individual strings of code... 
+// You can see we test the overall array with 2 strings first, and then another array as the 3rd value and this subarray contains 6 string tests, and 1 more new sub array test
+$fakephp->run(["s /lb/testing", "s (and)testing 2 new linebreak before/lb/", ["s testing sub array/lb/", "s test sub array 2/lb/", "s test new line
+testing here after newline", "s 
+test linebreak after empty first line/lb/", "s  extra space after 's' command before string", ["s hellloooo", "s /lb/ and there"]]]);
+
+
 ?>
