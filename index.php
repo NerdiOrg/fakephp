aa70e40260abf7392ef906928c63384067435352
NerdiOrg <38194792+NerdiOrg@users.noreply.github.com>
2021-09-29 13:49:41 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php
-function fakephp($string){
- $endofline = "<br>"; // use PHP_EOL for console / server outputs
-//echo "the string is".$string;
- $lines = explode( "\n",$string);
- //print_r($lines);
- $linemax = count($lines);
- $linecount = 0;
- foreach($lines as $line){
-  if(strpos($line, "s ") === 0){
-    $sayvalue = str_replace("s ", "", $line);
-    $endsay = $linecount < $linemax-1 ? $endofline  : "";
-    echo $sayvalue . $endsay;
-  }
-  $linecount++;
- }
-}
-$code = "s Hello!
-s How are you?";
-fakephp($code);
+require_once "FakePHP.php";
+$fakephp = new FakePHP();
+
+// COMMAND "s" : Performs an echo of anything after it, until another command is encountered
+// A command must be the first thing on a new line, with nothing before it, and at least 1 space after it
+
+// The line break between "Hello!" and "How are you?" is registered & will have a line break included in it:
+$fakephp->run("s Hello!
+How are you?"); 
+
+// Also, you can use "/lb/"  to force a line break:
+$fakephp->run("s /lb/Hello2 b!");
 ?>
